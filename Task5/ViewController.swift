f5cfc7b25e928f8c731fd6c05cf6369ed21b9979
miurakazuma67 <miurakazuma67@gmail.com>
2021-03-02 00:48:49 +0900

    1回目提出

--- Task5/ViewController.swift
+++ Task5/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    var alertController: UIAlertController!
+    
+    func alert(title:String, message:String) {
+        alertController = UIAlertController(title: title,
+                                            message: message,
+                                            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style: .default,
+                                                handler: nil))
+        present(alertController, animated: true)
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        guard let num1 = Double(textField1.text!) else {
+            alert(title: "課題5",
+                  message: "割られる数を入力してください。")
+            return
+        }
+        guard let num2 = Double(textField2.text!) else {
+            alert(title: "課題5",
+                  message: "割る数を入力してください。")
+            return
+        }
+        guard textField2.text != "0" else {
+            alert(title: "課題5",
+                  message: "割る数には0を入力しないでください。")
+            return
+        }
+        let result = num1 / num2
+        resultLabel.text = String(result)
+    }
 }
 
